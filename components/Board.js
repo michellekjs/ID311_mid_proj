c379185fce32f63a04405719f0d3c0e47d129107
michelle <michellekjs@kaist.ac.kr>
2022-05-12 01:54:30 +0900

    Working on player construction:

--- components/Board.js
+++ components/Board.js
@@ -13,7 +13,6 @@ class Cells {
 
   draw() {
     this.setup()
-    
   }
 }
 
--- components/Introduction.js
+++ components/Introduction.js
@@ -1,3 +1,26 @@
-class IntroPage {
-  
+let button ; 
+
+function setup(){
+  createCanvas(1000,1000)
+  button = createButton('click me');
+  button.position(0, 0);
+  button.mousePressed(moveMain);
+
+
+  let player1 = createInput('');
+  player1.position(100,100);
+  player1.size(100);
+
+  let player2 = createInput('');
+  player2.position(300,100);
+  player2.size(100);
 }
+
+function moveMain() {
+  window.location.href = "../game.html"
+
+}
+
+export {Player}
+
+window.setup = setup;
